b5f8947988f6de0fa8f3bc17600c55336f4a0b87
Amelia Baldwin <amelia01000010@gmail.com>
2022-08-21 18:21:10 +0100

    changes

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -2,49 +2,69 @@
 
 var screenNumber = document.getElementById('screen');
 var newCalcTrue = true;
-var prevOp = false; //// basic calculator functions go here
+var count = 0;
+var storeNum0 = 0;
+var storeNum1 = 0; //// basic calculator functions go here
 
-var calculate = function calculate(input) {////get string split up into equation?simplist way?
-  //screenNumber.forEach(item)
-  // if number put in box else add one put in next box
+var calculate = function calculate(input) {
+  calc = 0; ////get string split up into equation? simplist way? limit to one operator with count in op func if bigger than 0 then trigger calculate?
+
+  switch (operate) {
+    case '+':
+      calc = storeNum0 + storeNum1;
+      break;
+
+    case '-':
+      calc = storeNum0 - storeNum1;
+      break;
+
+    case '*':
+      calc = storeNum0 * storeNum1;
+      break;
+
+    case '-':
+      calc = storeNum0 / storeNum1;
+      break;
+  }
 };
 
-var inputFunc = function inputFunc(event) {
-  var input = item.innerHTML;
-  screenNumber.innerHTML += input;
-  console.log(input);
-  screenNumber.innerHTML += '7';
-};
-
-var onButton = document.getElementById('on'); ///sets value of screen to 0 when C is clicked
+var onButton = document.getElementById('on'); ///sets value of screen to 0 when C is clicked resets all variables
 
 onButton.addEventListener("click", function () {
   screenNumber.innerHTML = "0";
   newCalcTrue = true;
-  return newCalcTrue;
+  count = 0;
+  calcArray.length = 0;
+  storeNum0 = 0;
+  storeNum1 = 0;
 });
 var inputNumber = document.querySelectorAll('.number-button');
 inputNumber.forEach(function (item) {
-  prevOp = false;
   item.addEventListener("click", function (event) {
     if (newCalcTrue === false) {
-      screenNumber.innerHTML += item.innerHTML;
+      screenNumber.innerHTML += item.innerText;
     } else {
-      screenNumber.innerHTML = item.innerHTML;
+      screenNumber.innerHTML = item.innerText;
       newCalcTrue = false;
-      return newCalcTrue, prevOp;
     }
+
+    calcArray[count] += item.innerText;
+    console.log(calcArray);
   });
-});
+}); ///adds operator to screen upon button click
+
 var inputOperator = document.querySelectorAll('.operator-button');
 inputOperator.forEach(function (item) {
   item.addEventListener("click", function (event) {
-    screenNumber.innerHTML += item.innerHTML;
-    newCalcTrue = false; // console.log(screenNumber.innerHTML);
-    //  if(prevOp === false){
-    //      screenNumber.innerHTML += item.innerHTML;
-    //      prevOp=true;
-    //      return prevOp;
-    //   }
+    screenNumber.innerHTML += item.innerText;
+    newCalcTrue = false; //  let prevOp = screenNumber.innerText.slice(-1);
+    //  if (prevOp === '+' || prevOp === '-' || prevOp === '*' || prevOp === '/') {
+    //  screenNumber.innerText = screenNumber.innerText;
+    //  } else {
+    //    screenNumber.innerText += item.innerText;
+    //  }
   });
 });
+var equalsButton = document.getElementById('equals'); ///sets does the sum then displays the answer
+
+equalsButton.addEventListener("click", calculate);
--- index.js
+++ index.js
@@ -1,57 +1,75 @@
 
 const screenNumber = document.getElementById('screen');
-let newCalcTrue = true;
-let prevOp = false;
+let newCalcTrue = true; 
+let count =0;
+let storeNum0 = 0;
+let storeNum1 =0;
 
 //// basic calculator functions go here
 const calculate = (input) =>{
-    ////get string split up into equation?simplist way?
-
-    //screenNumber.forEach(item)
-    // if number put in box else add one put in next box
-    
-}
-
-const inputFunc = (event) =>{
-    const input = item.innerHTML;
-    screenNumber.innerHTML += input;
-    console.log(input);  
-    screenNumber.innerHTML += '7';
+    calc = 0
+    ////get string split up into equation? simplist way? limit to one operator with count in op func if bigger than 0 then trigger calculate?
+    switch(operate) {
+        case '+':
+          calc = storeNum0 + storeNum1;
+          break;
+        case '-':
+            calc = storeNum0 - storeNum1;
+          break;
+          case '*':
+            calc = storeNum0*storeNum1;
+            break;
+          case '-':
+            calc = storeNum0/storeNum1;
+            break;
+      }
 }
 
 const onButton = document.getElementById('on');
-///sets value of screen to 0 when C is clicked
+///sets value of screen to 0 when C is clicked resets all variables
 onButton.addEventListener("click", ()=>{
     screenNumber.innerHTML = "0";
     newCalcTrue = true;
-    return newCalcTrue;
+    count = 0;
+    calcArray.length = 0;
+    storeNum0 = 0;
+    storeNum1 =0;
 });
 
 const inputNumber = document.querySelectorAll('.number-button');
 inputNumber.forEach((item) =>{
-    prevOp = false;
     item.addEventListener("click",(event)=>{  
-    if(newCalcTrue === false){
-       screenNumber.innerHTML += item.innerHTML;
-    }
-    else{
-        screenNumber.innerHTML = item.innerHTML;
-        newCalcTrue = false;
-        return newCalcTrue, prevOp;
-    }})
+        if(newCalcTrue === false){
+            screenNumber.innerHTML += item.innerText;
+        }
+        else{
+            screenNumber.innerHTML = item.innerText;
+            newCalcTrue = false;
+        }
+        calcArray[count] += item.innerText;
+        console.log(calcArray);
+    })   
 })
 
+
+///adds operator to screen upon button click
  const inputOperator = document.querySelectorAll('.operator-button');
  inputOperator.forEach((item) =>{
      item.addEventListener("click",(event)=>{  
-    screenNumber.innerHTML += item.innerHTML;
-    newCalcTrue = false;
-  // console.log(screenNumber.innerHTML);
-  //  if(prevOp === false){
-  //      screenNumber.innerHTML += item.innerHTML;
-  //      prevOp=true;
-  //      return prevOp;
-  //   }
+    screenNumber.innerHTML += item.innerText;
+     newCalcTrue = false;
+
+
+    //  let prevOp = screenNumber.innerText.slice(-1);
+    //  if (prevOp === '+' || prevOp === '-' || prevOp === '*' || prevOp === '/') {
+    //  screenNumber.innerText = screenNumber.innerText;
+    //  } else {
+    //    screenNumber.innerText += item.innerText;
+    //  }
 
  })
  })
+
+ const equalsButton = document.getElementById('equals');
+///sets does the sum then displays the answer
+equalsButton.addEventListener("click", calculate);
