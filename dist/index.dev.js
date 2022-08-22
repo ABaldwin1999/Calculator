74f3c9aca884095dd3dcddd9c31798fe46eb5424
Amelia Baldwin <amelia01000010@gmail.com>
2022-08-22 15:15:03 +0100

    Javascript added

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -1,44 +1,57 @@
 "use strict";
 
-var screenNumber = document.getElementById('screen');
+var screenNumber = document.getElementById("screen");
 var newCalcTrue = true;
 var count = 0;
-var storeNum0 = 0;
-var storeNum1 = 0; //// basic calculator functions go here
+var storeNum0 = "";
+var storeNum1 = "";
+var currentOp = ""; //// basic calculator functions go here
 
-var calculate = function calculate(input) {
-  calc = 0; ////get string split up into equation? simplist way? limit to one operator with count in op func if bigger than 0 then trigger calculate?
+var calculate = function calculate() {
+  calc = 0;
+  Num0 = Number(storeNum0);
+  Num1 = Number(storeNum1);
 
-  switch (operate) {
-    case '+':
-      calc = storeNum0 + storeNum1;
+  switch (currentOp) {
+    case "+":
+      calc = Num0 + Num1;
       break;
 
-    case '-':
-      calc = storeNum0 - storeNum1;
+    case "-":
+      calc = Num0 - Num1;
       break;
 
-    case '*':
-      calc = storeNum0 * storeNum1;
+    case "*":
+      calc = Num0 * Num1;
       break;
 
-    case '-':
-      calc = storeNum0 / storeNum1;
+    case "/":
+      if (Num1 === 0) {
+        calc = NaN;
+      } else {
+        calc = Num0 / Num1;
+      }
+
       break;
   }
+
+  screenNumber.innerText = calc;
+  storeNum0 = "" + calc + "";
+  storeNum1 = "";
+  console.log("called!");
 };
 
-var onButton = document.getElementById('on'); ///sets value of screen to 0 when C is clicked resets all variables
+var onButton = document.getElementById("on"); ///sets value of screen to 0 when C is clicked resets all variables
 
 onButton.addEventListener("click", function () {
   screenNumber.innerHTML = "0";
   newCalcTrue = true;
   count = 0;
-  calcArray.length = 0;
-  storeNum0 = 0;
-  storeNum1 = 0;
+  storeNum0 = "";
+  storeNum1 = "";
+  currentOp = "";
 });
-var inputNumber = document.querySelectorAll('.number-button');
+var inputNumber = document.querySelectorAll(".number-button");
 inputNumber.forEach(function (item) {
   item.addEventListener("click", function (event) {
     if (newCalcTrue === false) {
@@ -48,23 +61,43 @@ inputNumber.forEach(function (item) {
       newCalcTrue = false;
     }
 
-    calcArray[count] += item.innerText;
-    console.log(calcArray);
+    if (count > 0) {
+      storeNum1 += item.innerText;
+    } else {
+      storeNum0 += item.innerText;
+    }
+
+    console.log(count);
   });
 }); ///adds operator to screen upon button click
 
-var inputOperator = document.querySelectorAll('.operator-button');
+var inputOperator = document.querySelectorAll(".operator-button");
 inputOperator.forEach(function (item) {
   item.addEventListener("click", function (event) {
-    screenNumber.innerHTML += item.innerText;
-    newCalcTrue = false; //  let prevOp = screenNumber.innerText.slice(-1);
-    //  if (prevOp === '+' || prevOp === '-' || prevOp === '*' || prevOp === '/') {
-    //  screenNumber.innerText = screenNumber.innerText;
-    //  } else {
-    //    screenNumber.innerText += item.innerText;
-    //  }
+    if (count > 0) {
+      calculate();
+      var prevOp = screenNumber.innerText.slice(-1);
+
+      if (prevOp === "+" || prevOp === "-" || prevOp === "*" || prevOp === "/") {
+        screenNumber.innerText = screenNumber.innerText;
+      } else {
+        screenNumber.innerText += item.innerText;
+      }
+    } else {
+      var _prevOp = screenNumber.innerText.slice(-1);
+
+      if (_prevOp === "+" || _prevOp === "-" || _prevOp === "*" || _prevOp === "/") {
+        screenNumber.innerText = screenNumber.innerText;
+      } else {
+        screenNumber.innerText += item.innerText;
+      }
+    }
+
+    newCalcTrue = false;
+    count++;
+    currentOp = item.innerText;
   });
 });
-var equalsButton = document.getElementById('equals'); ///sets does the sum then displays the answer
+var equalsButton = document.getElementById("equals"); ///sets does the sum then displays the answer
 
 equalsButton.addEventListener("click", calculate);
