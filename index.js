d0fc7cf0501743585a7de15cb91ccb4d37e4a4f6
Amelia Baldwin <amelia01000010@gmail.com>
2022-08-22 15:14:38 +0100

    Javacript added

--- index.js
+++ index.js
@@ -1,75 +1,105 @@
-
-const screenNumber = document.getElementById('screen');
-let newCalcTrue = true; 
-let count =0;
-let storeNum0 = 0;
-let storeNum1 =0;
+const screenNumber = document.getElementById("screen");
+let newCalcTrue = true;
+let count = 0;
+let storeNum0 = "";
+let storeNum1 = "";
+let currentOp = "";
 
 //// basic calculator functions go here
-const calculate = (input) =>{
-    calc = 0
-    ////get string split up into equation? simplist way? limit to one operator with count in op func if bigger than 0 then trigger calculate?
-    switch(operate) {
-        case '+':
-          calc = storeNum0 + storeNum1;
-          break;
-        case '-':
-            calc = storeNum0 - storeNum1;
-          break;
-          case '*':
-            calc = storeNum0*storeNum1;
-            break;
-          case '-':
-            calc = storeNum0/storeNum1;
-            break;
+const calculate = () => {
+  calc = 0;
+  Num0 = Number(storeNum0);
+  Num1 = Number(storeNum1);
+  switch (currentOp) {
+    case "+":
+      calc = Num0 + Num1;
+      break;
+    case "-":
+      calc = Num0 - Num1;
+      break;
+    case "*":
+      calc = Num0 * Num1;
+      break;
+    case "/":
+      if (Num1 === 0) {
+        calc = NaN;
+      } else {
+        calc = Num0 / Num1;
       }
-}
+      break;
+  }
+  screenNumber.innerText = calc;
+  storeNum0 = "" + calc + "";
+  storeNum1 = "";
+  console.log("called!");
+};
 
-const onButton = document.getElementById('on');
+const onButton = document.getElementById("on");
 ///sets value of screen to 0 when C is clicked resets all variables
-onButton.addEventListener("click", ()=>{
-    screenNumber.innerHTML = "0";
-    newCalcTrue = true;
-    count = 0;
-    calcArray.length = 0;
-    storeNum0 = 0;
-    storeNum1 =0;
+onButton.addEventListener("click", () => {
+  screenNumber.innerHTML = "0";
+  newCalcTrue = true;
+  count = 0;
+  storeNum0 = "";
+  storeNum1 = "";
+  currentOp = "";
 });
 
-const inputNumber = document.querySelectorAll('.number-button');
-inputNumber.forEach((item) =>{
-    item.addEventListener("click",(event)=>{  
-        if(newCalcTrue === false){
-            screenNumber.innerHTML += item.innerText;
-        }
-        else{
-            screenNumber.innerHTML = item.innerText;
-            newCalcTrue = false;
-        }
-        calcArray[count] += item.innerText;
-        console.log(calcArray);
-    })   
-})
-
+const inputNumber = document.querySelectorAll(".number-button");
+inputNumber.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    if (newCalcTrue === false) {
+      screenNumber.innerHTML += item.innerText;
+    } else {
+      screenNumber.innerHTML = item.innerText;
+      newCalcTrue = false;
+    }
+    if(count>0){
+      storeNum1 += item.innerText;
+    }
+    else{
+      storeNum0 += item.innerText;
+    }
+    console.log(count);
+  });
+});
 
 ///adds operator to screen upon button click
- const inputOperator = document.querySelectorAll('.operator-button');
- inputOperator.forEach((item) =>{
-     item.addEventListener("click",(event)=>{  
-    screenNumber.innerHTML += item.innerText;
-     newCalcTrue = false;
+const inputOperator = document.querySelectorAll(".operator-button");
+inputOperator.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    if ((count > 0)) {
+      calculate();
+      let prevOp = screenNumber.innerText.slice(-1);
+      if (
+        prevOp === "+" ||
+        prevOp === "-" ||
+        prevOp === "*" ||
+        prevOp === "/"
+      ) {
+        screenNumber.innerText = screenNumber.innerText;
+      } else {
+        screenNumber.innerText += item.innerText;
+      }
+    } else {
+      let prevOp = screenNumber.innerText.slice(-1);
+      if (
+        prevOp === "+" ||
+        prevOp === "-" ||
+        prevOp === "*" ||
+        prevOp === "/"
+      ) {
+        screenNumber.innerText = screenNumber.innerText;
+      } else {
+        screenNumber.innerText += item.innerText;
+      }
+    }
+    newCalcTrue = false;
+    count++;
+    currentOp = item.innerText;
+  });
+});
 
-
-    //  let prevOp = screenNumber.innerText.slice(-1);
-    //  if (prevOp === '+' || prevOp === '-' || prevOp === '*' || prevOp === '/') {
-    //  screenNumber.innerText = screenNumber.innerText;
-    //  } else {
-    //    screenNumber.innerText += item.innerText;
-    //  }
-
- })
- })
-
- const equalsButton = document.getElementById('equals');
+const equalsButton = document.getElementById("equals");
 ///sets does the sum then displays the answer
 equalsButton.addEventListener("click", calculate);
